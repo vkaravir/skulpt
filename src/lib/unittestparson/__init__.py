9857c6a5d27efe4a06606051fc514e830fd2220f
Ville Karavirta <vkaravir@gmail.com>
2013-10-02 11:49:10 +0300

    Rewriting of the parsons unittest to return a JSON string

--- src/lib/unittestparson/__init__.py
+++ src/lib/unittestparson/__init__.py
@@ -1,12 +1,26 @@
-import document
-import unittestgui
+__author__ = 'vkaravir'
 
-class unittest(unittestgui.unittest):
+class unittest():
+    """
+    A simple unittest class to be used with the js-parsons
+    exercise framework: https://github.com/vkaravir/js-parsons/
+    """
+    def __init__(self):
+        self.tlist = []
+        testNames = {}
+        for name in dir(self):
+            if name[:4] == 'test' and name not in testNames:
+                self.tlist.append(getattr(self,name))
+                testNames[name]=True
+
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
     def main(self):
-        l = document.createElement('ul')
-        self.resdiv.appendChild(l)
-        self.resList = l
-
+        self.result = []
         for func in self.tlist:
             try:
                 self.setup()
@@ -14,41 +28,92 @@ class unittest(unittestgui.unittest):
                 self.tearDown()
             except Exception as e:
                 self.appendErrorResult(e)
-                self.numFailed += 1
-                raise
-        self.showSummary()
+
+        # join the result array to return a JSON string
+        return '[' + ', '.join(self.result) + ']';
+
+    def assertEqual(self, actual, expected, feedback=""):
+        res = actual==expected and type(actual)==type(expected)
+        self.appendResult(res, actual, ' to be equal to ',expected, feedback)
+
+    def assertNotEqual(self, actual, expected, feedback=""):
+        res = actual != expected
+        self.appendResult(res, actual, ' to not equal ',expected,feedback)
+
+    def assertTrue(self,x, feedback=""):
+        res = x
+        self.appendResult(res, x, ' to be ',True,feedback)
+
+    def assertFalse(self,x, feedback=""):
+        res = not x
+        self.appendResult(res, x, ' to be ',False,feedback)
+
+    def assertIs(self,a,b, feedback=""):
+        res = a is b
+        self.appendResult(res, a, ' to be the same object as ',b,feedback)
+
+    def assertIsNot(self,a,b, feedback=""):
+        res = a is not b
+        self.appendResult(res, a, ' to not be the same object as ',b,feedback)
+
+    def assertIsNone(self,x, feedback=""):
+        res = x is None
+        self.appendResult(res, x, ' to be', None,feedback)
+
+    def assertIsNotNone(self,x, feedback=""):
+        res = x is not None
+        self.appendResult(res, x, ' to not be ',None,feedback)
+
+    def assertIn(self,a,b, feedback=""):
+        res = a in b
+        self.appendResult(res, a, ' to be in ',b,feedback)
+
+    def assertNotIn(self,a,b, feedback=""):
+        res = a not in b
+        self.appendResult(res, a, ' to not be in ',b,feedback)
+
+    def assertIsInstance(self,a,b, feedback=""):
+        res = isinstance(a,b)
+        self.appendResult(res, a, ' to be an instance of ',b,feedback)
+
+    def assertNotIsInstance(self,a,b, feedback=""):
+        res = not isinstance(a,b)
+        self.appendResult(res, a, ' to not be an instance of ',b,feedback)
+
+    def assertAlmostEqual(self,a,b, feedback=""):
+        res = round(a-b,7) == 0
+        self.appendResult(res, a, ' to equal ',b,feedback)
+
+    def assertNotAlmostEqual(self,a,b, feedback=""):
+        res = round(a-b,7) != 0
+        self.appendResult(res, a, ' to not equal ',b,feedback)
+
+    def assertGreater(self,a,b, feedback=""):
+        res = a > b
+        self.appendResult(res, a, ' to be greater than ',b,feedback)
+
+    def assertGreaterEqual(self,a,b, feedback=""):
+        res = a >= b
+        self.appendResult(res, a, ' to be greater than or equal to ',b,feedback)
+
+    def assertLess(self,a,b, feedback=""):
+        res = a < b
+        self.appendResult(res, a, ' to be less than ',b,feedback)
+
+    def assertLessEqual(self,a,b, feedback=""):
+        res = a <= b
+        self.appendResult(res, a, ' to be less than or equal to ',b,feedback)
 
     def appendErrorResult(self, error):
-        msg = '<div>Error in parsing/executing your program:</div><span class="error">' + str(error) + '</span>'
-        self.appendFeedbackMessage(msg, "error")
+        # generate a JSON string for this error
+        self.result.append('{"status":"error", "_error":"%s"}'%str(error))
 
-    def appendResult(self,res,actual,expected,feedback):
-        msg = '<div class="feedback">' + feedback + '</div>'
+    def appendResult(self,res,actual,comp, expected,feedback):
         if res:
-            self.numPassed += 1
             status = "pass"
         else:
-            msg += '<span class="expected">Expected %s %s</span>' % (str(actual),str(expected))
-            self.numFailed += 1
             status = "fail"
-        self.appendFeedbackMessage(msg, status)
-
-    def appendFeedbackMessage(self, msg, status):
-        pTag = document.createElement('li')
-        pTag.setAttribute("class", "testcase " + status)
-        pTag.innerHTML = msg
-        self.resList.appendChild(pTag)
-
-    def showSummary(self):
-        pct = self.numPassed / (self.numPassed+self.numFailed) * 100
-        pTag = document.createElement('p')
-        pTag.setAttribute("class", "summary")
-        pTag.innerHTML = "You passed: " + str(pct) + "% of the tests"
-        self.resdiv.appendChild(pTag)
-        if pct < 90:
-            self.resdiv.setCSS('background-color','#de8e96')
-        else:
-            self.resdiv.setCSS('background-color','#83d382')
-
-
-
+        # generate a JSON string for this result
+        jsonstr = '{"status": "%s", "expected": "%s", "actual": "%s", "test":"%s", "feedback":"%s"}' \
+                            %(status, str(expected), str(actual), comp, feedback)
+        self.result.append(jsonstr)
